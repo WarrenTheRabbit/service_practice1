ab626e0b8bdb41e8fb89e8523031e30a14a3bdbd
Warren <rabbitsinwarrens@gmail.com>
2024-07-31 19:12:55 +1000

    chore: group all walking skeleton tests in a child folder

--- service1/tests/test_walking_skeleton/test_api_server.py
+++ service1/tests/test_walking_skeleton/test_api_server.py
@@ -1,12 +1,22 @@
 import pytest
+import logging
+
+from src.logger import logger
 from fastapi.testclient import TestClient
 
 from src.main import app
 
 client = TestClient(app)
 
+
 def test_that_the_root_path_responds():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "This is the root of service1."}
     
+
+def test_that_requests_are_logged(caplog):
+    with caplog.at_level(logging.INFO, logger="service1"):
+        client.get("/")
+        assert "Logged a message" in caplog.text
+        
