17cea23d467f83648cd3d6baea600201be8b772f
Warren <rabbitsinwarrens@gmail.com>
2024-07-31 19:14:23 +1000

    feat(walking_skeleton): log a message from within an endpoint

--- service1/src/main.py
+++ service1/src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from src.logger import logger
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    logger.info("Logged a message")
     return {"msg": "This is the root of service1."}
 
 
